eb83a6c21e0bed54a5163e8189b770004d87e48a
natsuto6 <79001972+natsuto6@users.noreply.github.com>
2023-02-23 00:16:54 +0900

    Userテーブルにカラムを追加

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2023_02_20_092615) do
+ActiveRecord::Schema.define(version: 2023_02_22_151354) do
 
   create_table "books", force: :cascade do |t|
     t.string "title"
@@ -29,6 +29,10 @@ ActiveRecord::Schema.define(version: 2023_02_20_092615) do
     t.datetime "remember_created_at"
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
+    t.string "name"
+    t.integer "postal_code"
+    t.text "address"
+    t.text "self_introduction"
     t.index ["email"], name: "index_users_on_email", unique: true
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
