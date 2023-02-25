fc0fcf9ed5b63bac1554a3666317671a19f86efd
natsuto6 <79001972+natsuto6@users.noreply.github.com>
2023-02-25 14:53:22 +0900

    ユーザー一覧画面にページング処理を追加

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -4,7 +4,7 @@ class UsersController < ApplicationController
   before_action :set_user, only: %i[show]
 
   def index
-    @users = User.all
+    @users = User.order(:id).page(params[:page])
   end
 
   def show; end
