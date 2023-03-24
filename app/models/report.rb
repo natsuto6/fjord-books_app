0298aff847f519b4b260baf33b40bebf65e2c3c9
natsuto6 <79001972+natsuto6@users.noreply.github.com>
2023-03-24 22:17:33 +0900

    ユーザーと日報の関連付け

--- app/models/report.rb
+++ app/models/report.rb
@@ -1,4 +1,8 @@
 # frozen_string_literal: true
 
 class Report < ApplicationRecord
+  belongs_to :user
+  validates :user_id, presence: true
+  validates :title, presence: true
+  validates :content, presence: true
 end
