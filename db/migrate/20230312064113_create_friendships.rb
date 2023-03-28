6d07b1edbfd7e0f97fd13b09b5b534539afdf7ae
natsuto6 <79001972+natsuto6@users.noreply.github.com>
2023-03-28 22:17:43 +0900

    制約の設定を追加

--- db/migrate/20230312064113_create_friendships.rb
+++ db/migrate/20230312064113_create_friendships.rb
@@ -1,12 +1,10 @@
 class CreateFriendships < ActiveRecord::Migration[6.1]
   def change
     create_table :friendships do |t|
-      t.integer :follower_id
-      t.integer :followed_id
+      t.references :follower, null: false, foreign_key: { to_table: :users }
+      t.references :followed, null: false, foreign_key: { to_table: :users }
 
       t.timestamps
     end
-    add_index :friendships, :followed_id
-    add_index :friendships, [:follower_id, :followed_id], unique: true
   end
 end
--- db/schema.rb
+++ db/schema.rb
@@ -50,12 +50,12 @@ ActiveRecord::Schema.define(version: 2023_03_12_064113) do
   end
 
   create_table "friendships", force: :cascade do |t|
-    t.integer "follower_id"
-    t.integer "followed_id"
+    t.integer "follower_id", null: false
+    t.integer "followed_id", null: false
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
     t.index ["followed_id"], name: "index_friendships_on_followed_id"
-    t.index ["follower_id", "followed_id"], name: "index_friendships_on_follower_id_and_followed_id", unique: true
+    t.index ["follower_id"], name: "index_friendships_on_follower_id"
   end
 
   create_table "users", force: :cascade do |t|
@@ -76,4 +76,6 @@ ActiveRecord::Schema.define(version: 2023_03_12_064113) do
 
   add_foreign_key "active_storage_attachments", "active_storage_blobs", column: "blob_id"
   add_foreign_key "active_storage_variant_records", "active_storage_blobs", column: "blob_id"
+  add_foreign_key "friendships", "users", column: "followed_id"
+  add_foreign_key "friendships", "users", column: "follower_id"
 end
