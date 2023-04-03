1265a1f14cc460132d373f76bf2a376ca9d2ca1b
natsuto6 <79001972+natsuto6@users.noreply.github.com>
2023-04-03 23:53:35 +0900

    Commentとポリモーフィック関連付け

--- app/models/report.rb
+++ app/models/report.rb
@@ -2,6 +2,8 @@
 
 class Report < ApplicationRecord
   belongs_to :user
+  has_many :comments, as: :commentable, dependent: :destroy
+
   validates :user_id, presence: true
   validates :title, presence: true
   validates :content, presence: true
