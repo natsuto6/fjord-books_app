4fae8aec560f0fe3cb5ab377bea23832857790ba
natsuto6 <79001972+natsuto6@users.noreply.github.com>
2023-04-11 10:07:41 +0900

    リソースを単数形に、不要id削除

--- config/routes.rb
+++ config/routes.rb
@@ -6,6 +6,6 @@ Rails.application.routes.draw do
   resources :users, only: %i[index show] do
     resources :followings, only: %i[index], module: :users
     resources :followers, only: %i[index], module: :users
-    resource :friendships, only: %i[create destroy]
+    resource :friendship, only: %i[create destroy]
   end
 end
