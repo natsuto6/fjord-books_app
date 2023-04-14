afb0d67ab0db7c78b343a2ead0e072da0fcae979
natsuto6 <79001972+natsuto6@users.noreply.github.com>
2023-04-15 00:21:45 +0900

    コメントの削除機能と編集機能の追加

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -1,6 +1,10 @@
 # frozen_string_literal: true
 
 class CommentsController < ApplicationController
+  before_action :set_comment, only: %i[edit update destroy]
+
+  def edit; end
+
   def create
     @comment = @commentable.comments.build(comment_params)
     @comment.user = current_user
@@ -12,7 +16,25 @@ class CommentsController < ApplicationController
     end
   end
 
+  def update
+    if @comment.update(comment_params)
+      redirect_to @commentable, notice: t('controllers.common.notice_update', name: Comment.model_name.human)
+    else
+      render :edit
+    end
+  end
+
+  def destroy
+    @comment.destroy
+
+    redirect_to @commentable, notice: t('controllers.notice_destroy', name: Comment.model_name.human)
+  end
+
   private
+
+  def set_comment
+    @comment = Comment.find(params[:id])
+  end
 
   def comment_params
     params.require(:comment).permit(:content)
--- config/routes.rb
+++ config/routes.rb
@@ -1,12 +1,12 @@
 Rails.application.routes.draw do
-  resources :reports do
-    resources :comments, only: %i[create], module: :reports
-  end
   mount LetterOpenerWeb::Engine, at: "/letter_opener" if Rails.env.development?
   devise_for :users
   root to: 'books#index'
   resources :books do
-    resources :comments, only: %i[create], module: :books
+    resources :comments, only: %i[create destroy edit update], module: :books
+  end
+  resources :reports do
+    resources :comments, only: %i[create destroy edit update], module: :reports
   end
   resources :users, only: %i[index show] do
     resource :relationships, only: %i[create destroy]
