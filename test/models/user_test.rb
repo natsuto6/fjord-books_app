7cb2b8a9c3ad747bc148d4349a253fcdc1e81d3f
natsuto6 <79001972+natsuto6@users.noreply.github.com>
2023-05-07 18:58:54 +0900

    Userクラスのテストを修正

--- test/models/user_test.rb
+++ test/models/user_test.rb
@@ -33,21 +33,13 @@ class UserTest < ActiveSupport::TestCase
     assert_not @user.followed_by?(user_non_follower)
   end
 
-  test 'follow should create a relationship' do
-    user_to_follow = users(:two)
-
-    assert_difference '@user.active_relationships.count', 1 do
-      @user.follow(user_to_follow)
-    end
-  end
-
   test 'unfollow should destroy a relationship' do
     user_to_unfollow = users(:two)
     @user.follow(user_to_unfollow)
+    assert @user.following?(user_to_unfollow)
 
-    assert_difference '@user.active_relationships.count', -1 do
-      @user.unfollow(user_to_unfollow)
-    end
+    @user.unfollow(user_to_unfollow)
+    assert_not @user.following?(user_to_unfollow)
   end
 
   test 'name_or_email should return name if present' do
